4b1df71a20c83533f0a60607cb7f5cd2d33621cb
AbraaoDev <abraaohomualdo@gmail.com>
2021-11-03 10:45:39 -0300

    :rocket: Desafio Completo!

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,37 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle){
+      return
+    }
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(oldState => [...oldState, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const completed = tasks.map(task => task.id === id ? {
+      ... task,
+      isComplete: !task.isComplete,
+    }: task)
+
+    setTasks(completed)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filterTask = tasks.filter(task => task.id !== id)
+
+    setTasks(filterTask)
   }
 
   return (
